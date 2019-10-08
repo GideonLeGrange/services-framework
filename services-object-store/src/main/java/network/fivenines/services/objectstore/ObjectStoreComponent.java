53b05f95d0e6665fc8d0da1becbe0e7428cb8d46
Gideon Le Grange <gideon@legrange.me>
2019-10-08 13:40:22 +0200

    Fixed bugs in JSON object store

--- services-object-store/src/main/java/network/fivenines/services/objectstore/ObjectStoreComponent.java
+++ services-object-store/src/main/java/network/fivenines/services/objectstore/ObjectStoreComponent.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
-import com.google.gson.stream.JsonWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -41,7 +40,7 @@ public class ObjectStoreComponent<S extends Service> extends Component<S, Object
         return "objectStore";
     }
 
-    public <T> T read(String key, Class<T> type) throws ObjectStoreException {
+    public <T> T readObject(String key, Class<T> type) throws ObjectStoreException {
         File file = new File(fileName(key));
         if (!file.exists()) {
             throw new ObjectStoreException(format("File '%s' does not exist", file.getAbsolutePath()));
@@ -53,7 +52,11 @@ public class ObjectStoreComponent<S extends Service> extends Component<S, Object
             throw new ObjectStoreException(format("File '%s' does is not readable", file.getAbsolutePath()));
         }
         try {
-            return gson.fromJson(new FileReader(file), type);
+            T data = gson.fromJson(new FileReader(file), type);
+            if (data == null) {
+                throw new ObjectStoreException(format("Could not read any data from '%s'", file.getAbsolutePath()));
+            }
+            return data;
         } catch (FileNotFoundException ex) {
             throw new ObjectStoreException(format("File '%s' was not found", file.getAbsolutePath()));
         } catch (JsonIOException ex) {
@@ -63,7 +66,7 @@ public class ObjectStoreComponent<S extends Service> extends Component<S, Object
         }
     }
 
-    public <T> void write(String key, T data) throws ObjectStoreException {
+    public <T> void writeObject(String key, T data) throws ObjectStoreException {
         File file = new File(fileName(key));
         if (!file.exists()) {
             if (!file.getParentFile().exists()) {
@@ -73,17 +76,25 @@ public class ObjectStoreComponent<S extends Service> extends Component<S, Object
                     throw new ObjectStoreException(format("Cannot create directory '%s' (%s)", file.getParent(), ex.getMessage()));
                 }
             }
-        }
-        if (!file.canWrite()) {
-            throw new ObjectStoreException(format("Cannot write to file directory '%s'", file.getPath()));
+        } else {
+            if (!file.canWrite()) {
+                throw new ObjectStoreException(format("Cannot write to file directory '%s'", file.getPath()));
+            }
         }
         try {
-            gson.toJson(data, data.getClass(), new JsonWriter(new FileWriter(file)));
+            try (FileWriter out = new FileWriter(file)) {
+                out.write(gson.toJson(data));
+            }
         } catch (IOException ex) {
             throw new ObjectStoreException(format("IO error writing to file '%s' (%s)", file.getPath(), ex.getMessage()));
         } catch (JsonIOException ex) {
             throw new ObjectStoreException(format("JSON IO error writing to file '%s' (%s)", file.getPath(), ex.getMessage()));
         }
+    }
+
+    public boolean hasObject(String key) {
+        File file = new File(fileName(key));
+        return file.exists() && file.isFile() && file.canRead();
     }
 
     private String fileName(String key) {
