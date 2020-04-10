e41ddbf592f579515bb58a02ff894a0cbc565070
Gideon Le Grange <gideon@legrange.me>
2020-04-10 23:32:56 +0200

    Skeleton for Let's Encrypt component created

--- service-letsencrypt/src/main/java/me/legrange/services/letsencrypt/LetsEncryptComponent.java
+++ service-letsencrypt/src/main/java/me/legrange/services/letsencrypt/LetsEncryptComponent.java
@@ -5,9 +5,14 @@ import me.legrange.service.ComponentException;
 import me.legrange.service.Service;
 import me.legrange.services.jetty.WithJetty;
 
+import java.io.File;
+
+import static java.lang.String.format;
+
 public final class LetsEncryptComponent  extends Component<Service, LetsEncryptConfig> implements WithJetty {
 
     private static LetsEncryptComponent instance;
+    private LetsEncryptConfig config;
 
     public LetsEncryptComponent(Service service) {
         super(service);
@@ -15,7 +20,14 @@ public final class LetsEncryptComponent  extends Component<Service, LetsEncryptC
 
     @Override
     public void start(LetsEncryptConfig config) throws ComponentException {
+        this.config = config;
         jetty().addEndpoint("/.well-known/acme-challenge/", ChallengeEndpoint.class);
+        if (hasCertificate()) {
+            service().submit(this::scheduleRenewalCheck);
+        }
+        else {
+            service().submit(this::obtainCertificate);
+        }
     }
 
     @Override
@@ -29,6 +41,24 @@ public final class LetsEncryptComponent  extends Component<Service, LetsEncryptC
 
     static LetsEncryptComponent getInstance() {
         return instance;
+    }
+
+    private void obtainCertificate() {
+
+    }
+
+    private void scheduleRenewalCheck() {
+
+    }
+
+    /** Determine if a certificate exists for the domain being served. */
+    private boolean hasCertificate() {
+        return new File(getCertificateFileName()).exists();
+    }
+
+    /** Make the certificate file name */
+    private String getCertificateFileName() {
+        return format("%s/%s.crt", config.getDataDirectory(), config.getDomain());
     }
 
 }
--- service-letsencrypt/src/main/java/me/legrange/services/letsencrypt/LetsEncryptConfig.java
+++ service-letsencrypt/src/main/java/me/legrange/services/letsencrypt/LetsEncryptConfig.java
@@ -1,4 +1,27 @@
 package me.legrange.services.letsencrypt;
 
+import javax.validation.constraints.NotBlank;
+
 public class LetsEncryptConfig {
+
+    @NotBlank(message = "The Let's Encrypt data directory needs to be specified")
+    private String dataDirectory;
+    @NotBlank(message = "The domain for which to manage certificates")
+    private String domain;
+
+    public String getDataDirectory() {
+        return dataDirectory;
+    }
+
+    public void setDataDirectory(String dataDirectory) {
+        this.dataDirectory = dataDirectory;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
 }
