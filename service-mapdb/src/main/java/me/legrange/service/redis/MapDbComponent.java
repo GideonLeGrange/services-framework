8d8df94efd3ddbcbdd308d0da507556a0ef4b14a
Gideon Le Grange <gideon@legrange.me>
2021-02-11 18:58:05 +0200

    Added MapDB support

---
+++ service-mapdb/src/main/java/me/legrange/service/redis/MapDbComponent.java
@@ -0,0 +1,37 @@
+package me.legrange.service.redis;
+
+import me.legrange.service.Component;
+import me.legrange.service.ComponentException;
+import me.legrange.service.Service;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+
+/**
+ * A component that provides access to MapDB.
+ */
+public class MapDbComponent extends Component<Service, MapDbConfig> {
+
+    private DB db;
+
+    public MapDbComponent(Service service) {
+        super(service);
+    }
+
+    @Override
+    public void start(MapDbConfig config) throws ComponentException {
+        DBMaker.Maker maker = DBMaker.fileDB(config.getDatabaseFile());
+        if (config.isMemoryMapped()) {
+            maker = maker.fileMmapEnable();
+        }
+        maker.closeOnJvmShutdown().make();
+    }
+
+    @Override
+    public String getName() {
+        return "jedis";
+    }
+
+    DB mapDb() {
+        return db;
+    }
+}
